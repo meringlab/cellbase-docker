0b8b814d95ff0e04b8850ef0f0cf889ace4f0d0c
Milan Simonovic <mbsimonovic@gmail.com>
2017-01-12 11:55:37 +0100

    update configs

--- build-cellbase.sh
+++ build-cellbase.sh
@@ -3,6 +3,6 @@
 chmod u+x ./bin/cellbase.sh 
 DATA='all'
 OPTS=" --conf configuration.json --data $DATA "
-./bin/cellbase.sh download $OPTS --species "Danio rerio" --output ensembl
-./bin/cellbase.sh build    $OPTS --species "Danio rerio" --input ensembl/danio_rerio_zv9/ --common ensembl/common/ --output drerio/
+./bin/cellbase.sh download $OPTS --species "Danio rerio" --assembly Zv9 --output ensembl
+./bin/cellbase.sh build    $OPTS --species drerio  -a Zv9 --input ensembl/danio_rerio_zv9/ --common ensembl/common/ --output drerio/
 ./bin/cellbase.sh load     $OPTS --input drerio/ --database cellbase_drerio_zv9_v3
