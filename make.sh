9c04abf09b9eab135b44b1ce2961bfea8630aa54
Milan Simonovic <mbsimonovic@gmail.com>
2017-01-17 15:56:49 +0100

    bugfix: pin to host, use unused port

--- make.sh
+++ make.sh
@@ -15,7 +15,8 @@ sudo docker build -t opencb/cellbase_web  -f Dockerfile.cellbase_web  .
 
 docker network create --driver overlay --subnet 10.1.5.0/24 geneassassin-network
 # pull the image on the required host and don't pass --with-registry-auth if in swarm mode so it only runs there, then:
-docker service create --name cellbase_mongo_v4 --network=geneassassin-network -p 31117:27017 -p 31118:28017 docker-registry.meringlab.org:5443/opencb/mongodb
+#docker service create --name cellbase_mongo_v4 --network=geneassassin-network -p 31117:27017 -p 31118:28017 docker-registry.meringlab.org:5443/opencb/mongodb
+docker service create --name cellbase_mongo_v4 --network=geneassassin-network --constraint "node.hostname == imlslnx-titan.uzh.ch" -p 31117:27017 -p 31118:28017 docker-registry.meringlab.org:5443/opencb/mongodb
 
 # setup local ensembl mysql: 
 for species in caenorhabditis_elegans_core_85_250 danio_rerio_core_85_10 saccharomyces_cerevisiae_core_85_4 mus_musculus_core_85_38;
@@ -37,4 +38,4 @@ sudo docker run --rm --link=ensembl-db -v /mnt/local/gene-assassin/load_backup/f
 docker run -d --add-host cb_mongo:130.60.240.80 --rm --name cellbase_load -v /mnt/local/gene-assassin/load_backup/ensembl:/opt/cellbase/build/ensembl -v /mnt/local/gene-assassin/load_backup/build:/opt/cellbase/build/build/ opencb/cellbase_load
 # or for interactive run: sudo docker run -it --add-host cb_mongo:130.60.240.80  --name cellbase_load opencb/cellbase_load bash
 
-docker service create --name cellbase_web_v4 --network=geneassassin-network -p 18180:8080  docker-registry.meringlab.org:5443/opencb/cellbase_web
+docker service create --name cellbase_web_v4 --network=geneassassin-network -p 18181:8080  docker-registry.meringlab.org:5443/opencb/cellbase_web
